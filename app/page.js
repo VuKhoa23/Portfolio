858604406ff2859521905ac752aa457be65b6bba
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-05-29 00:02:24 +0700

    Edit ' for eslint

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import classes from './page.module.css'
+import classes from "./page.module.css"
  
 export default function Home() {
   return (
@@ -6,9 +6,9 @@ export default function Home() {
         <section className={classes.introduce}> 
             <p1>Hi !</p1>
             <p>Im Khoa, a software engineer student.</p>
-            <p>This is where I showcase my projects and what I've learned from them.</p>
+            <p>This is where I showcase my projects and what I&apos;ve learned from them.</p>
         </section>
-        <img src={'/cropped.jpg'} alt='Portfolio Image' height='500rem' />
+        <img src={"/cropped.jpg"} alt="Portfolio Image" height="500rem" />
     </main>
   );
 }

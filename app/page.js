9e6507efa3dfaa9975d58d3e0af4dc8ace1dac3c
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-05-28 20:16:43 +0700

    Setting up bootstrap

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from 'next/link'
 // server side codes
 export default function Home() {
   return (
-      <>
-        Hello
-      </>
+    <>
+      <h1>Hello</h1>
+      <Link href="/about">Go to about</Link>
+    </>
   );
 }

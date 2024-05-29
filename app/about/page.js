f664894b75d39da2046a7a0c98dfb91586ad489c
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-05-29 09:47:48 +0700

    Add background image

--- app/about/page.js
+++ app/about/page.js
@@ -1,20 +1,21 @@
 import classes from "./page.module.css";
 import Image from "next/image";
-import profilePic from "../../public/me.jpg"
+import profilePic from "../../public/me-nobg.png"
 export const metadata = {
   title: "Khoa Vu - About",
   description: "Porfolio",
 };
 
+
 export default function About() {
   return (
     <container className={classes.main + " row"}>
-      <div className={classes["image-container"] + "col col-xl-5 col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center"}>
-        <div style={{height: 30 + "rem", width: 25 + 'rem', position: "relative"}}>
+      <div className={classes["image-container"] + " col-12 col-xl-5 col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center"}>
+        <div className="image" style={{height: 100 + "%", width: 50 + '%', position: "relative"}}>
             <Image src={profilePic} alt="Portfolio Image" layout="fill"/>
         </div>
       </div>
-      <main className={"col col-xl-7 col-lg-12 col-md-12 col-sm-12"}>
+      <main className={"col-12 col-xl-7 col-lg-12 col-md-12 col-sm-12"}>
         <div>
           <section className={classes["introduce-section"]}>
             <p1>About me</p1>
--- app/layout.js
+++ app/layout.js
@@ -3,18 +3,30 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import BootstrapClient from "@/components/BootstrapClient";
 import Header from "@/components/header";
+import backgroundImg from "../public/bg.png"
+import Image from "next/image";
 
 export const metadata = {
   title: "Khoa Vu",
   description: "Porfolio",
 };
 
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <div className="bg">
+          <Image
+            src={backgroundImg}
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
         <Header />
-        {children} 
+        {children}
         <BootstrapClient />
       </body>
     </html>

161f25a2935246ab11b40afa37e84ad8e91f6975
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-05-29 14:57:00 +0700

    FInsihed about page

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,4 +1,4 @@
-import classes from './page.module.css'
+import classes from "./page.module.css"
 
 export const metadata = {
   title: "Khoa Vu - Projects",
@@ -8,13 +8,17 @@ export const metadata = {
 export default function Projects() {
   return (
     <>
-      <container className={classes.main + " row"}>
-        <div className={"col-12 " + classes.project}>
-          <h3>Project 1</h3>
+      <container className={classes.main + " row d-flex flex-row justify-content-around"}>
+        <div className={"col-xxl-5 col-xl-7 col-md-10 " + classes.project}>
+          <div className={classes["project-header"] + " d-flex flex-row justify-content-between align-items-center"}>
+          <h3>E-Commerce WebApp</h3>
+          <p>Aug 21 - Dec 22</p>
+          </div>
           <p>Sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample</p>
         </div>
-        <div className="col-8">
-          <h3>Project 2</h3>
+        <div className={"col-xxl-5 col-xl-7 col-md-10 " + classes.project}>
+          <h3>Project 1</h3>
+          <p>Sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample</p>
         </div>
       </container>
     </>

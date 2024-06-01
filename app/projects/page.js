8a5054d4d6d7140a8541cc44c8a097c6441658be
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-06-01 11:40:13 +0700

    Add new project

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,9 +1,10 @@
-import classes from "./page.module.css"
+import classes from "./page.module.css";
 import Project from "./project";
-import portfolioProjectImg from "../../public/images/portfolio-project.png"
-import weshoppingProjectImg from "../../public/images/weshopping-project.png"
-import chatProjectImg from "../../public/images/chat-project.png"
-import weadsProjectImg from "../../public/images/weads-project.png"
+import portfolioProjectImg from "../../public/images/portfolio-project.png";
+import weshoppingProjectImg from "../../public/images/weshopping-project.png";
+import chatProjectImg from "../../public/images/chat-project.png";
+import weadsProjectImg from "../../public/images/weads-project.png";
+import wnwProjectImg from "../../public/images/wnw-project.png";
 
 import Image from "next/image";
 export const metadata = {
@@ -13,50 +14,104 @@ export const metadata = {
 
 const project = {
   title: "Portfolio Website",
-  description: <p>A portfolio website to showcase what I have done and learned, as well as to easily reach out to recruiters.</p>,
-  img: <Image src={portfolioProjectImg} layout="fill" alt="Portfolio Website Image" />,
+  description: (
+    <p>
+      A portfolio website to showcase what I have done and learned, as well as
+      to easily reach out to recruiters.
+    </p>
+  ),
+  img: (
+    <Image
+      src={portfolioProjectImg}
+      layout="fill"
+      alt="Portfolio Website Image"
+    />
+  ),
   youtube: undefined,
   github: "https://github.com/VuKhoa23/Portfolio",
-  techStack: "NextJs (React), Bootstrap"
-}
+  techStack: "NextJs (React), Bootstrap",
+};
 
 const project2 = {
   title: "WeShopping",
-  description: <p>Simple E-Commerce Website. This project serves as a culmination of my Java Spring knowledge, and concurrently learning Angular to develop the frontend</p>,
-  img: <Image src={weshoppingProjectImg} layout="fill" alt="Portfolio Website Image" />,
+  description: (
+    <p>
+      Simple E-Commerce Website. This project serves as a culmination of my Java
+      Spring knowledge, and concurrently learning Angular to develop the
+      frontend
+    </p>
+  ),
+  img: (
+    <Image
+      src={weshoppingProjectImg}
+      layout="fill"
+      alt="Portfolio Website Image"
+    />
+  ),
   youtube: "https://www.youtube.com/watch?v=PuaeUYqUh_Q",
   github: "https://github.com/VuKhoa23/WeShopping",
-  techStack: "Java Spring, Angular, Bootstrap, MySQL"
-}
+  techStack: "Java Spring, Angular, Bootstrap, MySQL",
+};
 
 const project3 = {
   title: "WeAds",
-  description: <p>WeAds empowers officials to supervise billboard placements
-    and manage personnel. Citizens access billboard information and can report issues or provide feedback.</p>,
-  img: <Image src={weadsProjectImg} layout="fill" alt="Portfolio Website Image" />,
+  description: (
+    <p>
+      WeAds empowers officials to supervise billboard placements and manage
+      personnel. Citizens access billboard information and can report issues or
+      provide feedback.
+    </p>
+  ),
+  img: (
+    <Image src={weadsProjectImg} layout="fill" alt="Portfolio Website Image" />
+  ),
   youtube: "https://www.youtube.com/watch?v=6SL3_YXreL0",
   github: "https://github.com/VuKhoa23/WE-ADS",
-  techStack: "ExpressJs (NodeJs), EJS, Bootstrap, MongoDB"
-}
+  techStack: "ExpressJs (NodeJs), MapBox API, EJS, Bootstrap, MongoDB",
+};
 
 const project4 = {
-    title: "Java Chat Application",
-    description: <p>Java Chat Application. User can chat 1v1, create group chat, send files and images.</p>,
-    img: <Image src={chatProjectImg} layout="fill" alt="Portfolio Website Image" />,
-    youtube: "https://www.youtube.com/watch?v=l5mrJ43TOqU",
-    github: "https://github.com/VuKhoa23/Java-Chat-App",
-    techStack: "Java Swing, Socket, MySQL"
-  }
+  title: "Java Chat Application",
+  description: (
+    <p>
+      Java Chat Application. User can chat 1v1, create group chat, send files
+      and images.
+    </p>
+  ),
+  img: (
+    <Image src={chatProjectImg} layout="fill" alt="Portfolio Website Image" />
+  ),
+  youtube: "https://www.youtube.com/watch?v=l5mrJ43TOqU",
+  github: "https://github.com/VuKhoa23/Java-Chat-App",
+  techStack: "Java Swing, Socket, MySQL",
+};
 
+const project5 = {
+  title: "Where And When - Trip Manager App",
+  description: (
+    <p>
+      My capstone project for Harvard CS50W certificate. A web application used to manage upcoming trips and archive completed ones.
+    </p>
+  ),
+  img: (
+    <Image src={wnwProjectImg} layout="fill" alt="Portfolio Website Image" />
+  ),
+  youtube: "https://www.youtube.com/watch?v=u6Rn7b067Q0",
+  github: "https://github.com/VuKhoa23/WhereAndWhen",
+  techStack: "Python Django, OpenWeatherMap API, SQLite",
+};
 
 export default function Projects() {
   return (
     <>
-      <container className={classes.main + " row d-flex flex-row justify-content-around"}>
-        <Project project={project} />
+      <container
+        className={classes.main + " row d-flex flex-row justify-content-around"}
+      >
         <Project project={project2} />
+        <Project project={project5} />
         <Project project={project3} />
         <Project project={project4} />
+        <Project project={project} />
       </container>
     </>
   );
